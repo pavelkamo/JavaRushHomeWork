d68d6e60c7a25e7b99ca386647ac8b5539706e80
pavelkamo <pavelkamo@windowslive.com>
2015-12-10 10:54:29 +0200

    level03.lesson04.task01

---
+++ src/com/javarush/test/level03/lesson04/task01/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level03.lesson04.task01;
+
+/* Дата рождения
+Вывести на экран дату своего рождения в виде: MAY 1 2012
+*/
+
+import java.util.Date;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String date = "MAY 1 2012";//напишите тут ваш код
+        System.out.printf(date);
+    }
+}
