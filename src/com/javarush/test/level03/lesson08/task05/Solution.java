f4cdf5780bb4cc67fcc629f009062223cd91e791
d.nechaev <d.nechaev@lucky-labs.com>
2015-12-10 10:21:45 +0200

    level03.lesson08.task05

--- src/com/javarush/test/level03/lesson08/task05/Solution.java
+++ src/com/javarush/test/level03/lesson08/task05/Solution.java
@@ -12,7 +12,11 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name1 = br.readLine();
+        String name2 = br.readLine();
+        String name3 = br.readLine();
+        System.out.println(name1 + " + " + name2 + " + " + name3 + " = " + "Чистая любовь, да-да!");
 
     }
 }
