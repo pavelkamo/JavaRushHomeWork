7a6b16b5bbbd772e0a5016c9260280e0caa8fb37
d.nechaev <d.nechaev@lucky-labs.com>
2015-12-10 10:44:11 +0200

    3 tasks done

---
+++ src/com/javarush/test/level02/lesson03/task01/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level02.lesson03.task01;
+
+/* Коты - это хорошо
+Создать объект типа Cat 2 раза.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Cat cat = new Cat();
+        cat = new Cat();//напишите тут ваш код
+    }
+
+    public static class Cat
+    {
+
+    }
+}
