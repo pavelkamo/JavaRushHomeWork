9cf05e1465a17695c444ebfa518e1e43f1fcc492
pavelkamo <pavelkamo@windowslive.com>
2016-03-11 02:20:32 +0200

    com.javarush.test.level04.lesson07.task04;

---
+++ src/com/javarush/test/level04/lesson07/task04/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level04.lesson07.task04;
+
+/* Положительные и отрицательные числа
+Ввести с клавиатуры три целых числа. Вывести на экран количество положительных и количество отрицательных чисел в исходном наборе,
+в следующем виде:
+"количество отрицательных чисел: а", "количество положительных чисел: б", где а, б - искомые значения.
+Пример для чисел 2 5 6:
+количество отрицательных чисел: 0
+количество положительных чисел: 3
+Пример для чисел -2 -5 6:
+количество отрицательных чисел: 2
+количество положительных чисел: 1
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int array [] = new int[3];
+        int positive = 0;
+        int negative = 0;
+        for(int i = 0; i < array.length; i++){
+            array[i] = Integer.parseInt(reader.readLine());//напишите тут ваш код
+            if (array[i] > 0){
+                positive++;
+            } else {
+                negative++;
+            }
+        }
+        System.out.println("количество отрицательных чисел: " + negative);
+        System.out.println("количество положительных чисел: " + positive);
+    }
+}
