ad68e7316d654ef5644af72635e8c70fd5ae3193
pavelkamo <pavelkamo@windowslive.com>
2016-03-17 01:19:34 +0200

    com.javarush.test.level05.lesson12.home03;

---
+++ src/com/javarush/test/level05/lesson12/home03/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.test.level05.lesson12.home03;
+
+/* Создай классы Dog, Cat, Mouse
+Создай классы Dog, Cat, Mouse. Добавь по три поля в каждый класс, на твой выбор. Создай объекты для героев мультика Том и Джерри. Так много, как только вспомнишь.
+Пример:
+Mouse jerryMouse = new Mouse(“Jerry”, 12 , 5), где 12 - высота в см, 5 - длина хвоста в см.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Mouse jerryMouse = new Mouse("Jerry", 12 , 5);
+
+        Mouse unkle = new Mouse("unkle", 77, 4);
+        Mouse jerryBrother = new Mouse("Bro", 2, 1);
+        Cat tom = new Cat("Tom", 3, 4);
+        Cat bomzh = new Cat("Bomzh", 5, 3);
+        Dog buldog = new Dog("Buldog", 88, 4);
+    }
+
+    public static class Mouse
+    {
+        String name;
+        int height;
+        int tail;
+
+        public Mouse(String name, int height, int tail)
+        {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Cat
+    {
+        String name;
+        int height;
+        int tail;
+
+        public Cat(String name, int height, int tail)
+        {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Dog
+    {
+        String name;
+        int height;
+        int tail;
+
+        public Dog(String name, int height, int tail)
+        {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+    //добавьте тут ваши классы
+
+}
