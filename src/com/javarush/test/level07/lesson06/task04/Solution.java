e63f89162dadb25ec24bf3f21262b7eafc7213be
pavelkamo <pavelkamo@windowslive.com>
2016-04-19 17:32:24 +0300

    level 7 lesson06

---
+++ src/com/javarush/test/level07/lesson06/task04/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level07.lesson06.task04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/* 10 строчек в начало списка
+1. Создай список строк в методе main.
+2. Добавь в него 10 строчек с клавиатуры, но только добавлять не в конец списка, а в начало.
+3. Используя цикл выведи содержимое на экран, каждое значение с новой строки.
+*/
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            arrayList.add(0, reader.readLine());
+        }
+
+        for (String s: arrayList){
+            System.out.println(s);
+        }
+    }
+}
