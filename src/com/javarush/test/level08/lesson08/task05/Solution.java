6868f006184b59ba043751406e6a0373120f1716
pavelkamo <pavelkamo@windowslive.com>
2016-09-08 20:22:57 +0300

    Level 9

--- src/com/javarush/test/level08/lesson08/task05/Solution.java
+++ src/com/javarush/test/level08/lesson08/task05/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level08.lesson08.task05;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /* Удалить людей, имеющих одинаковые имена
 Создать словарь (Map<String, String>) занести в него десять записей по принципу «фамилия» - «имя».
@@ -9,45 +10,15 @@ import java.util.*;
 
 public class Solution
 {
-    public static void main(String[] args) {
-        HashMap<String, String> stringHashMap = createMap();
-        for (Map.Entry<String, String> map : stringHashMap.entrySet()){
-            System.out.println(map.getKey() + " - " + map.getValue());
-        }
-        System.out.println("========");
-        removeTheFirstNameDuplicates(stringHashMap);
-        System.out.println("========");
-        for (Map.Entry<String, String> map : stringHashMap.entrySet()){
-            System.out.println(map.getKey() + " - " + map.getValue());
-        }
-    }
-
     public static HashMap<String, String> createMap()
     {
-        HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("lastName0", "name0");
-        hashMap.put("lastName1", "name");
-        hashMap.put("lastName2", "name2");
-        hashMap.put("lastName3", "name3");
-        hashMap.put("lastName4", "name");
-        hashMap.put("lastName5", "name5");
-        hashMap.put("lastName6", "name6");
-        hashMap.put("lastName7", "name");
-        hashMap.put("lastName8", "name8");
-        hashMap.put("lastName9", "name9");
-
-        return hashMap;
+        //напишите тут ваш код
+        return new HashMap<String, String>();
     }
 
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map)
     {
-        HashMap<String, String> newMap = new HashMap<>();
-        HashSet<String> hashSet = new HashSet<>();
-        for (Map.Entry<String, String> s : map.entrySet()){
-            if (!hashSet.contains(s.getValue())) {
-                hashSet.add(s.getValue());
-            }
-        }
+        //напишите тут ваш код
 
     }
 
